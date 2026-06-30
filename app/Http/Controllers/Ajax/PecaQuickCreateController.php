2e3cde1ffcc6b8b234b6765939cc4d00d50e1c6b
mcp26 <marcellopessoa@gmail.com>
2026-06-30 17:02:03 -0300

    Auto-update via Claude Code

--- app/Http/Controllers/Ajax/PecaQuickCreateController.php
+++ app/Http/Controllers/Ajax/PecaQuickCreateController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Ajax;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StorePecaRequest;
+use App\Models\Peca;
 
 class PecaQuickCreateController extends Controller
 {
-    //
+    public function store(StorePecaRequest $request)
+    {
+        $peca = Peca::create($request->validated());
+
+        return response()->json(['id' => $peca->id, 'nome' => $peca->nome], 201);
+    }
 }
