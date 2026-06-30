6ba57b67f561cd292bcb24666b8d668b343914ab
mcp26 <marcellopessoa@gmail.com>
2026-06-30 16:50:56 -0300

    Auto-update via Claude Code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'master' => \App\Http\Middleware\EnsureUserIsMaster::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
